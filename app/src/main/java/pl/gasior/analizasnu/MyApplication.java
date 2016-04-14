246a8aed7821c64f5e39f4711841b7b2c93df9a7
Piotr Gasior <pgasior94@gmail.com>
2016-04-14 16:53:46 +0200

    ACRA

--- app/src/main/java/pl/gasior/analizasnu/MyApplication.java
+++ app/src/main/java/pl/gasior/analizasnu/MyApplication.java
@@ -6,9 +6,13 @@ import android.content.Context;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
+import org.acra.ACRA;
+import org.acra.annotation.ReportsCrashes;
+
 /**
  * Created by Piotrek on 04.04.2016.
  */
+@ReportsCrashes(mailTo = "analizasnu.bledy@gmail.com")
 public class MyApplication extends Application {
 //    public static RefWatcher getRefWatcher(Context context) {
 //        MyApplication application = (MyApplication) context.getApplicationContext();
@@ -16,6 +20,12 @@ public class MyApplication extends Application {
 //    }
 
     private RefWatcher refWatcher;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.init(this);
+    }
 
     @Override public void onCreate() {
         super.onCreate();
