dc957c4a14b00b06588d9b02d5551d1cb24466f5
Piotr Gasior <pgasior94@gmail.com>
2016-05-30 16:33:39 +0200

    Informacja o tym ze analiza moze chwile potrwać

--- app/src/main/java/pl/gasior/analizasnu/ui/ConfirmAnalysisDialogFragment.java
+++ app/src/main/java/pl/gasior/analizasnu/ui/ConfirmAnalysisDialogFragment.java
@@ -37,7 +37,7 @@ public class ConfirmAnalysisDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Oznaczenia zostaną usunięte")
+        builder.setMessage("Analiza może chwilę potrwać, w zależności od długości nagrania. Oznaczenia zostaną usunięte")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
